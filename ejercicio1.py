7d30a2d1a9e346ae26249fb30d9922c06b02acfb
Oswaldo Quinatoa <sharkiller1361994@gmail.com>
2018-03-26 11:08:24 -0500

    Prueba commit 1

---
+++ ejercicio1.py
@@ -0,0 +1,37 @@
+def maximo(num1,num2):
+	if num1>num2:
+		print 'el numero '+str(num1)+' es mayor a '+str(num2)
+	else:
+		print 'el numero '+str(num2)+' es mayor a '+str(num1)
+
+def maximo3numeros(num1,num2,num3):
+	if num1>num2 and num1>num3:
+		print 'el numero '+str(num1)+' es mayor a '+str(num2)+' y mayor a '+str(num3)
+	elif num2>num1 and num2>num3:
+		print 'el numero '+str(num2)+' es mayor a '+str(num1)+' y mayor a '+str(num3)
+
+	else:
+		print 'el numero '+str(num3)+' es mayor a '+str(num2)+' y mayor a '+str(num1)
+
+#maximo3numeros(7,9,6)
+def lentarreglo(arreglo):
+	print "formar 1"+str(len(arreglo))
+	count=0
+	for data in arreglo:
+		
+		count=count+1
+		print data
+	print "Forma 2:"+str(count)
+
+def devulvevocal(palabra):
+	palabra=palabra.strip(' ')
+	if palabra=='a' or palabra=='e' or palabra=='i' or palabra=='o' or palabra=='u':
+		print 'si es una vocal'
+	else:
+		print 'no es vocal'
+
+	
+arre={"s","dd",'sssss'}
+#lentarreglo(arre)
+devulvevocal('e   ')
+
